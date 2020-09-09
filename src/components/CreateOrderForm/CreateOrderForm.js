8ac47f97e90d34fe7c01f3cea0f512ee4b7e9d85
Vitali <vskrabatun@gmail.com>
2020-09-09 17:48:04 +0300

    feat: add CreateOrderForm component

---
+++ src/components/CreateOrderForm/CreateOrderForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CreateOrderForm = () => {
+    return (        
+        <div className="wrapper-product-form wrapper-pop-up-form">
+            <div className="create-product-form pop-up-form">
+                <h3>Please, write product details</h3>
+                <form action="#">
+                    <input type="text" className="input-name" placeholder="Name" required />
+                    <input type="text" className="input-price" placeholder="Price" required />
+                    <input type="submit" className="submit-create-product" value="Create Product" />
+                </form>     
+                <div className="product-cancel-button cancel-button"></div> 
+            </div>
+        </div>
+    )
+}
+
+export default CreateOrderForm;
